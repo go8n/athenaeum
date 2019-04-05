39f42bb48377ae818cf6686b0c130d5dddaa3b9b
librebob <librebob@protonmail.com>
2019-04-05 15:42:07 +1000

    Create QtDbus notify.py

---
+++ athenaeum/notify.py
@@ -0,0 +1,35 @@
+from PyQt5.QtCore import QVariant, QMetaType
+from PyQt5.QtDBus import QDBusConnection, QDBusArgument, QDBusInterface, QDBus
+
+class Notify:
+    def __init__(self, name='', *args, **kwargs):
+        self._appName = name
+        
+        self._bus = QDBusConnection.sessionBus()
+        if not self._bus.isConnected():
+            print("Not connected to dbus!")
+        
+    def show_notifitcation(self, header, message, icon):
+        item = "org.freedesktop.Notifications"
+        path = "/org/freedesktop/Notifications"
+        interface = "org.freedesktop.Notifications"
+
+        v = QVariant(12321)  # random int to identify all notifications
+        if v.convert(QVariant.UInt):
+            id_replace = v
+
+        actions_list = QDBusArgument([], QMetaType.QStringList)
+        hint = {}
+        
+        time = 100
+
+        notify = QDBusInterface(item, path, interface, self._bus)
+        if notify.isValid():
+            x = notify.call(QDBus.AutoDetect, "Notify", self._appName,
+                            id_replace, icon, header, message,
+                            actions_list, hint, time)
+            if x.errorName():
+                print("Failed to send notification!")
+                print(x.errorMessage())
+        else:
+            print("Invalid dbus interface")
