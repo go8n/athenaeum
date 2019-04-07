edcc1cbd1d04c5a52fb5ff8950d041ee2e705c5d
Rampoina <RampoinaG@gmail.com>
2019-04-07 17:44:02 +0200

    Add Catalan translation

--- athenaeum.pro
+++ athenaeum.pro
@@ -5,4 +5,5 @@ TRANSLATIONS = athenaeum/translations/athenaeum_la.ts \
                athenaeum/translations/athenaeum_zh_Hant.ts \
                athenaeum/translations/athenaeum_sv.ts \
                athenaeum/translations/athenaeum_nb_NO.ts \
-               athenaeum/translations/athenaeum_sr_Latn.ts
+               athenaeum/translations/athenaeum_sr_Latn.ts \
+               athenaeum/translations/athenaeum_ca.ts
---
+++ athenaeum/translations/athenaeum_ca.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca_ES">
+<context>
+    <name>LibraryView</name>
+    <message>
+        <location filename="../LibraryView.qml" line="30"/>
+        <source>Search %L1 Games...</source>
+        <translation>Cerca %L1 jocs...</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="45"/>
+        <source>Library</source>
+        <translation>Biblioteca</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="51"/>
+        <source>⋮</source>
+        <translation>⋮</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="61"/>
+        <source>Settings</source>
+        <translation>Paràmetres</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="65"/>
+        <source>Check For Updates</source>
+        <translatorcomment>Shortened the translation a bit so that it fits</translatorcomment>
+        <translation>Comprova actualitzacions</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="69"/>
+        <source>Update All</source>
+        <translation>Actualitza-ho tot</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="73"/>
+        <source>Exit</source>
+        <translation>Sortir</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="91"/>
+        <source>You have operations pending.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="108"/>
+        <source>Close Anyway</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="174"/>
+        <source>All Games (%L1)</source>
+        <translation>Tots els jocs</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="175"/>
+        <source>Installed (%L1)</source>
+        <translation>Instal·lats</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="176"/>
+        <source>Recent (%L1)</source>
+        <translation>Recents</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="177"/>
+        <source>New (%L1)</source>
+        <translation>Nous</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="178"/>
+        <source>Has Updates (%L1)</source>
+        <translation>Conté actualitzacions</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="179"/>
+        <source>Processing (%L1)</source>
+        <translation>Processant</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="291"/>
+        <source>New</source>
+        <translation>Nou</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="402"/>
+        <source>Install</source>
+        <translation>Instal·lar</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="420"/>
+        <source>Play</source>
+        <translation>Jugar</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="438"/>
+        <source>Update</source>
+        <translation>Actualitzar</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="451"/>
+        <source>Uninstall</source>
+        <translation>Desinstal·lar</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="484"/>
+        <source>Are you sure?</source>
+        <translation>Estàs segur?</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="502"/>
+        <source>Yes</source>
+        <translation>Sí</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="122"/>
+        <location filename="../LibraryView.qml" line="516"/>
+        <source>Cancel</source>
+        <translation>Cancel·lar</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="715"/>
+        <source>Description</source>
+        <translation>Descripcció</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="742"/>
+        <source>Releases</source>
+        <translation>Publicacions</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="765"/>
+        <source>Version %1</source>
+        <translation>Versió %1</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="811"/>
+        <source>Developer</source>
+        <translation>Desenvolupador</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="844"/>
+        <source>License</source>
+        <translation>Llicència</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="876"/>
+        <source>Links</source>
+        <translation>Enllaços</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="918"/>
+        <source>Homepage</source>
+        <translation>Pàgina principal</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="920"/>
+        <source>Bug Tracker</source>
+        <translation>Seguidor d&apos;errades</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="922"/>
+        <source>Help</source>
+        <translation>Ajuda</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="924"/>
+        <source>FAQ</source>
+        <translation>FAQ</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="926"/>
+        <source>Donate</source>
+        <translation>Fer un donatiu</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="928"/>
+        <source>Translation</source>
+        <translation>Traducció</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="930"/>
+        <source>Unknown</source>
+        <translation>Desconegut</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="932"/>
+        <source>Manifest</source>
+        <translation>Manifest</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="956"/>
+        <source>Categories</source>
+        <translation>Categories</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="999"/>
+        <source>Installed successfully.</source>
+        <translation>S&apos;ha instal·lat correctament.</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="1001"/>
+        <source>Uninstalled successfully.</source>
+        <translation>S&apos;ha desinstal·lat correctament.</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="1003"/>
+        <source>Updated successfully.</source>
+        <translation>S&apos;ha actualitzat correctament.</translation>
+    </message>
+    <message>
+        <location filename="../LibraryView.qml" line="1005"/>
+        <source>An error occurred.</source>
+        <translation>S&apos;ha produït un error.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsView</name>
+    <message>
+        <location filename="../SettingsView.qml" line="21"/>
+        <source>‹</source>
+        <translation>‹</translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="41"/>
+        <source>Settings</source>
+        <translation>Paràmetres</translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="50"/>
+        <source>⋮</source>
+        <translation>⋮</translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="66"/>
+        <source>Reset All</source>
+        <translation>Restableix-ho tot</translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="69"/>
+        <source>Exit</source>
+        <translation>Sortir</translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="90"/>
+        <source>Show Tray Icon</source>
+        <translation>Mostra una icona a la safata del sistema</translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="104"/>
+        <source>Always Show Logs</source>
+        <translation>Sempre mostrar els registres</translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="118"/>
+        <source>Notifications Enabled</source>
+        <translation>Notificacions habilitades</translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="126"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+</context>
+</TS>
