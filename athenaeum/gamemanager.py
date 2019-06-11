34bcd0a77254292dfdc3702c03e4170e8ed57cf0
librebob <librebob@protonmail.com>
2019-06-11 12:01:42 +1000

    add more callbacks

--- athenaeum/gamemanager.py
+++ athenaeum/gamemanager.py
@@ -70,11 +70,11 @@ class GameManager(QObject):
                 process.start('flatpak', ['install', 'flathub', self._games[index].ref, '-y', '--user'])
             self._processes.append(process)
 
-    def installStarted(self, index, startedCallback=None):
+    def installStarted(self, index, callback=None):
         self._games[index].processing = True
-        if startedCallback: startedCallback()
+        if callback: callback()
 
-    def installFinished(self, process, index, finishedCallback=None):
+    def installFinished(self, process, index, callback=None):
         self._games[index].processing = False
 
         if process.exitCode():
@@ -88,7 +88,7 @@ class GameManager(QObject):
         self._games[index].appendLog(process, finished=True)
         self._gameRepository.set(self._games[index])
         self.processCleanup(process, index, action)
-        if finishedCallback: finishedCallback()
+        if callback: callback()
 
     def uninstallGame(self, game_id, startedCallback=None, finishedCallback=None):
         index = self.findById(game_id)
@@ -104,11 +104,11 @@ class GameManager(QObject):
                 process.start('flatpak', ['uninstall', self._games[index].ref, '-y', '--user'])
             self._processes.append(process)
 
-    def uninstallStarted(self, index, startedCallback=None):
+    def uninstallStarted(self, index, callback=None):
         self._games[index].processing = True
-        if startedCallback: startedCallback()
+        if callback: callback()
 
-    def uninstallFinishd(self, process, index, finishedCallback=None):
+    def uninstallFinishd(self, process, index, callback=None):
         self._games[index].processing = False
 
         if process.exitCode():
@@ -122,9 +122,9 @@ class GameManager(QObject):
         self._games[index].appendLog(process, finished=True)
         self._gameRepository.set(self._games[index])
         self.processCleanup(process, index, action)
-        if finishedCallback: finishedCallback()
+        if callback: callback()
 
-    def updateGame(self, game_id):
+    def updateGame(self, game_id, startedCallback=None, finishedCallback=None):
         index = self.findById(game_id)
         if index is not None:
             print('update')
@@ -157,12 +157,12 @@ class GameManager(QObject):
         self._gameRepository.set(self._games[index])
         self.processCleanup(process, index, action)
 
-    def playGame(self, game_id):
+    def playGame(self, game_id, startedCallback=None, finishedCallback=None):
         index = self.findById(game_id)
         if index is not None:
             process = QProcess(parent=self.parent())
-            process.started.connect(partial(self.startGame, index))
-            process.finished.connect(partial(self.stopGame, process, index))
+            process.started.connect(partial(self.startGame, index, startedCallback))
+            process.finished.connect(partial(self.stopGame, process, index, finishedCallback))
             process.readyReadStandardOutput.connect(partial(self._games[index].appendLog, process))
             process.readyReadStandardError.connect(partial(self._games[index].appendLog, process))
             if self._flatpak:
@@ -171,16 +171,18 @@ class GameManager(QObject):
                 process.start('flatpak', ['run', self._games[index].ref])
             self._processes.append(process)
 
-    def startGame(self, index):
+    def startGame(self, index, callback):
         self._games[index].playing = True
         self._games[index].lastPlayedDate = datetime.now()
+        if callback: callback()
 
-    def stopGame(self, process, index):
+    def stopGame(self, process, index, callback):
         self._games[index].playing = False
         self._games[index].lastPlayedDate = datetime.now()
         self._games[index].appendLog(process, finished=True)
         self._gameRepository.set(self._games[index])
         self.processCleanup(process, index)
+        if callback: callback()
 
     def searchGames(self):
         if self.searchValue:
--- athenaeum/library.py
+++ athenaeum/library.py
@@ -12,8 +12,8 @@ class Library(QObject):
     gamesChanged = pyqtSignal()
     filterChanged = pyqtSignal()
     filtersChanged = pyqtSignal(list)
-    filterValueChanged = pyqtSignal()
-    searchValueChanged = pyqtSignal()
+    paramsChanged = pyqtSignal()
+    paramsChanged = pyqtSignal()
     currentGameChanged = pyqtSignal()
     errorChanged = pyqtSignal()
 
@@ -24,25 +24,24 @@ class Library(QObject):
         self.reset()
 
     def load(self):
-        self.filterValue = self._metaRepository.get('filter') or 'installed'
         self.updateFilters(new_load=True)
-        self.currentGame = self.filter[0] if len(self._filter) else Game()
+        self.filterValue = self._metaRepository.get('filter') or 'installed'
+        self.currentGame = self.filter[0] if len(self.filter) else Game()
 
     def reset(self):
-        self._filter = []
         self._filters = {
             'installed': [],
             'recent': [],
             'has_updates': [],
             'processing': []
         }
-        self._filterValue = ''
+        self._filterValue = 'installed'
         self._searchValue = ''
         self._currentGame = Game()
 
     @pyqtSlot(result=int)
     def getIndexForCurrentGame(self):
-        for index, game in enumerate(self._filter):
+        for index, game in enumerate(self.filter):
             if game.id == self._currentGame.id:
                 return index
             
@@ -53,12 +52,17 @@ class Library(QObject):
 
     @pyqtSlot(str)
     def updateCurrentGame(self, game_id):
+        for game in self.filter:
+            if game.id == game_id:
+                self.currentGame = game
+                return
+        
         for key, games in self._filters.items():
             for game in games:
                 if game.id == game_id:
                     self.currentGame = game
                     self.filterValue = key
-                    self.filterGames()
+                    print(key)
                     return
 
     @pyqtSlot(str, result=int)
@@ -94,27 +98,22 @@ class Library(QObject):
     def processingCount(self):
         return len(self._filters['processing'])
 
-    @pyqtProperty(QQmlListProperty, notify=filterChanged)
+    @pyqtProperty(QQmlListProperty, notify=paramsChanged)
     def filter(self):
-        return QQmlListProperty(Game, self, self._filter)
+        return QQmlListProperty(Game, self, list(filter(lambda x: self._searchValue.lower() in x.name.lower(), self._filters[self._filterValue])))
 
-    @filter.setter
-    def filter(self, filter):
-        if filter != self._filter:
-            self._filter = filter
-            self.filterChanged.emit()
-
-    @pyqtProperty('QString', notify=filterValueChanged)
+    @pyqtProperty('QString', notify=paramsChanged)
     def filterValue(self):
         return self._filterValue
 
     @filterValue.setter
     def filterValue(self, filterValue):
-        if filterValue != self._filterValue:
-            self._filterValue = filterValue
-            self.filterValueChanged.emit()
+        #if filterValue != self._filterValue:
+        self._filterValue = filterValue
+        self._metaRepository.set(key='filter', value=self.filterValue)
+        self.paramsChanged.emit()
 
-    @pyqtProperty('QString', notify=searchValueChanged)
+    @pyqtProperty('QString', notify=paramsChanged)
     def searchValue(self):
         return self._searchValue
 
@@ -122,40 +121,13 @@ class Library(QObject):
     def searchValue(self, searchValue):
         if searchValue != self._searchValue:
             self._searchValue = searchValue
-            self.searchValueChanged.emit()
+            self.paramsChanged.emit()
 
     def indexUpdated(self, index):
         try:
-            self.currentGame = self._filter[index]
+            self.currentGame = self.filter[index]
         except IndexError:
             print('Index does not exist.')
-
-    def installGame(self, game_id):
-        self._gameManager.installGame(game_id, startedCallback=self.updateFilters, finishedCallback=self.updateFilters)
-
-    def uninstallGame(self, game_id):
-        self._gameManager.uninstallGame(game_id, finishedCallback=self.updateFilters)
-
-    def updateGame(self, game_id):
-        self._gameManager.updateGame(game_id)
-
-    def playGame(self, game_id):
-        self._gameManager.playGame(game_id)
-
-    def searchGames(self):
-        if self.searchValue:
-            tmp = []
-            query = self.searchValue.lower()
-            for game in self._filters[self.filterValue]:
-                if query in game.name.lower():
-                    tmp.append(game)
-            self.filter = tmp
-        else:
-            self.filterGames()
-
-    def filterGames(self):
-        self.filter = self._filters[self.filterValue]
-        self._metaRepository.set(key='filter', value=self.filterValue)
 
     def updateFilters(self, new_load=False):
         filters = {
@@ -173,11 +145,22 @@ class Library(QObject):
                 filters['processing'].append(game)
             if game.hasUpdate:
                 filters['has_updates'].append(game)
-            if game.lastPlayedDate:
+            if game.lastPlayedDate and game.installed:
                 if (game.lastPlayedDate + timedelta(days=15)) > now:
                     filters['recent'].append(game)
 
         self._filters = filters
-        self.filterGames()
-        self.searchGames()
-        self.filtersChanged.emit(self._filters['recent'][:5] or self._filters['installed'][:5])
+        self.paramsChanged.emit()
+        self.filtersChanged.emit([])
+
+    def installGame(self, game_id):
+        self._gameManager.installGame(game_id, startedCallback=self.updateFilters, finishedCallback=self.updateFilters)
+
+    def uninstallGame(self, game_id):
+        self._gameManager.uninstallGame(game_id, startedCallback=self.updateFilters, finishedCallback=self.updateFilters)
+
+    def updateGame(self, game_id):
+        self._gameManager.updateGame(game_id, startedCallback=self.updateFilters, finishedCallback=self.updateFilters)
+
+    def playGame(self, game_id):
+        self._gameManager.playGame(game_id, startedCallback=self.updateFilters, finishedCallback=self.updateFilters)
